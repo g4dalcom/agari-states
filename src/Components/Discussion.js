c6c1b243bbe1438d6814eda03b82d64dc46dc832
g4dalcom <g4dalcom@gmail.com>
2023-04-06 23:00:10 +0900

    데이터 불러오기 완료

--- src/Components/Discussion.js
+++ src/Components/Discussion.js
@@ -1,12 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Discussion = () => {
-  useEffect(() => {});
+  const [discussion, setDiscussion] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/')
+      .then((res) => res.json())
+      .then((data) => setDiscussion(data));
+  }, []);
 
   return (
     <>
       <section className='discussion__wrapper'>
-        <ul className='discussions__container'></ul>
+        <ul className='discussions__container'>
+          {discussion.map((el) => {
+            return (
+              <li className='discussion__container'>
+                <div className='discussion__avatar--wrapper'>
+                  <img
+                    className='discussion__avatar--image'
+                    src={el.avatarUrl}
+                    alt={`avatar of ${el.author}`}
+                  />
+                </div>
+                <div className='discussion__content'>
+                  <h2 className='discussion__title'></h2>
+                  <a href={el.url} rel='noreferrer' target='_blank'>
+                    {el.title}
+                  </a>
+                  <div className='discussion__information'>
+                    {el.author} / {el.createdAt.slice(0, 10)}
+                  </div>
+                </div>
+                <div className='discussion__answered'>
+                  <p className={el.answer && 'complete'}>
+                    {el.answer ? '답변완료' : '해결중'}
+                  </p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
         <div className='pagination_container'></div>
       </section>
     </>
