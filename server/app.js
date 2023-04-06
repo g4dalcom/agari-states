382cf762374cd91dcccb59086646960783827f64
g4dalcom <g4dalcom@gmail.com>
2023-04-06 19:09:23 +0900

    router 구현

--- server/app.js
+++
@@ -1,9 +0,0 @@
-const { discussitonController } = require('../server/controller');
-const { findAll, findById } = discussitonController;
-const express = require('express');
-const router = express.Router();
-
-router.get('/', findAll);
-router.get('/:id', findById);
-
-module.exports = router;
